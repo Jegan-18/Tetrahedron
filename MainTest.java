c9aa65e852e4ca40aac284cfdc866e020206ad05
Jegan-18 <99559917+Jegan-18@users.noreply.github.com>
2022-03-14 14:40:33 +0530

    Add files via upload

---
+++ MainTest.java
@@ -0,0 +1,10 @@
+package com.cg.test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		TetrahedronPyramid c1 = new TetrahedronPyramid(5);
+		System.out.println("The area is:" + c1.area(5));
+	}
+}
---
+++ TetrahedronPyramid.java
@@ -0,0 +1,24 @@
+package com.cg.test;
+
+public class TetrahedronPyramid {
+
+	private int s;
+
+	public TetrahedronPyramid(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getS() {
+		return s;
+	}
+
+	public void setS(int s) {
+		this.s = s;
+	}
+
+	public static double area(int s) {
+		return (Math.sqrt(3) * (s * s));
+
+	}
+
+}
